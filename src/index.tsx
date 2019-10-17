a3d7ec6b75268a3908d5084109844135c8cc9423
morphy brown <codiechanel@gmail.com>
2019-10-17 21:22:19 +0800

    the best

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import Members from "./components/Members";
 import { Router, Link } from "@reach/router";
 import LeftContainer from "./components/LeftContainer";
 import RightContainer from "./components/RightContainer";
+
 const HorizontalBox = styled.div`
   display: flex;
   width: 100vw;
@@ -16,65 +17,32 @@ const HorizontalBox = styled.div`
   justify-content: space-between;
   flex: 1;
 `;
-const Nav = styled.div`
-  background-color: rebeccapurple;
+
+type NavProp = {
+  // primary?: string;
+  flex: string;
+};
+
+const Nav = styled.div<NavProp>`
   /* we are allowed margins here because we dont use 100 */
-  margin: 10px;
-  padding: 10px;
-
-  flex: 1;
+  /* flex: 1; */
+  flex: ${props => props.flex};
   display: flex;
-  /*  this needs to be a row */
-  flex-direction: row;
+  /*  this needs to be a row 
+  but we dont have to declare it because 
+  its a flex default */
 `;
-const Nav2 = styled.div`
-  margin: 0px;
-  padding: 0px;
-  flex: 1;
-  /* this should be removed because it shouldn't handle the scrollbar */
-  overflow: auto;
 
-  display: flex;
-  flex-direction: column;
-  /* flex-direction: row; */
-  align-items: stretch;
-  /* align items in Cross Axis */
-  height: 100vh;
-  width: 100vw;
-  /* width: 100%; */
-
-  /*height: 100%;*/
-
-  scrollbar-color: #40566f rgba(0, 0, 0, 0.15);
-  /* not sure */
-  /* position: relative; */
-`;
-const Article = styled.div`
-  /* we are allowed margins here because we dont use 100 */
+const LeftSection = styled(Nav)`
   margin: 10px;
-  padding: 10px;
   background-color: rebeccapurple;
-
-  /* here we could use an arbitrary number based on screen size */
-  flex: 2;
-  display: flex;
-  /* row or column works here */
-  flex-direction: row;
-  /* flex-direction: column; */
 `;
-const Article2 = styled.div`
-  flex: 2;
-  flex-direction: column;
-  display: flex;
-  /* align items in Cross Axis */
-  /* not really needed */
-  /* align-items: stretch;  */
 
-  /* I tried using vw and it broke the layout showing the scrollbar */
-  /* width: 100vw; */
-  /* height: 100vh; */
-  /* width: 100%;
-  height: 100%; */
+const RightSection = styled(Nav)`
+  margin-top: 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  background-color: rebeccapurple;
 `;
 
 let useMedia = query => {
@@ -95,62 +63,33 @@ function App() {
   let small = useMedia("(min-width: 600px)");
   let large = useMedia("(min-width: 1000px)");
   let content = (
-    <Nav>
+    <LeftSection flex="1">
       <Router>
         <LeftContainer path="/" />
         <RightContainer path="/dashboard" />
       </Router>
-    </Nav>
+    </LeftSection>
   );
   /* if screen is larger than 600, then lets display 2 panels */
   if (small) {
     content = (
       <>
-        <Nav>
+        <LeftSection flex="1">
           <LeftContainer path="/" />
-        </Nav>
+        </LeftSection>
         {/* {content} */}
-        <Article>
+        <RightSection flex="2">
           {/* <ui.RightPanel>
             <Members />
           </ui.RightPanel> */}
           <RightContainer path="/dashboard" />
-        </Article>
+        </RightSection>
       </>
     );
   }
-  return <HorizontalBox>{content}</HorizontalBox>;
+  // return <HorizontalBox>{content}</HorizontalBox>;
+  return content;
 }
-/* return (
-  <div className="Media">
-    <h1>Media</h1>
-    <p>Small? {small ? "Yep" : "Nope"}.</p>
-    <p>Large? {large ? "Yep" : "Nope"}.</p>
-  </div>
-); */
-/* class App extends React.Component<any, any> {
-  //   constructor(props) {
-  //     super(props);
-  //   }
-
-  componentDidMount() {
-    console.log("the best");
-  }
-
-  render() {
-    let small = useMedia("(min-width: 600px)");
-    let large = useMedia("(min-width: 1000px)");
-    console.log(small, large);
-    return (
-      <HorizontalBox>
-        <Nav>welcome the best hey now</Nav>
-        <Article>
-          <Hello />
-        </Article>
-      </HorizontalBox>
-    );
-  }
-} */
 
 ReactDOM.render(
   <App />,
